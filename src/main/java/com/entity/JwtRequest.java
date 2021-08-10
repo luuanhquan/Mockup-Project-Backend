1915098e1e03bb51af52b8a2f8bac56cfa331afc
Lucas Luu <51409135+luuanhquan@users.noreply.github.com>
2021-08-11 00:29:46 +0700

    encode password

--- src/main/java/com/entity/JwtRequest.java
+++ src/main/java/com/entity/JwtRequest.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.Serializable;
+import java.util.Base64;
 
 @Getter
 @Setter
@@ -15,4 +18,15 @@ public class JwtRequest implements Serializable {
 
     private String username;
     private String password;
+    public String getPassword(){
+        return new String(Base64.getDecoder().decode(password));
+    }
+
+    @Override
+    public String toString() {
+        return "JwtRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
